4ffd939ba70cc48c52447b9456faf4983169b5f5
Calvin <calvin@edu.ink>
2022-04-25 14:15:02 +0800

    style: basic calendar layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Box, Card } from '@mui/material';
+import moment from 'moment';
+import { useEffect, useMemo, useState } from 'react';
 import './App.css';
 
+interface DayCard {
+  label: string;
+  key: string;
+  state: string;
+  dayOfWeek: string;
+}
+
 function App() {
+  /** Day of week headers */
+  const headers: string[] = useMemo(() => {
+    const result: string[] = [];
+    let date = moment().startOf("week");
+    for (let i = 0; i < 7; i++) {
+      result.push(date.format("ddd"));
+      date.add(1, 'day');
+    }
+    return result;
+  }, []);
+
+  /** Day cards */
+  const [dayCards, setDayCards] = useState<DayCard[]>([]);
+  const [activeDate, setActiveDate] = useState<Date>(new Date());
+  useEffect(() => {
+    const newValue: DayCard[] = [];
+    const stopDate = moment(activeDate).endOf("month");
+    for (const cursor = moment(activeDate).startOf("month"); cursor.isBefore(stopDate); cursor.add(1, "day")) {
+      newValue.push({
+        label: cursor.format("D"),
+        key: cursor.toISOString(),
+        state: "normal",
+        dayOfWeek: cursor.format("e")
+      })
+    }
+    setDayCards(newValue)
+  }, [activeDate])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Box
+        sx={{
+          display: 'grid',
+          gap: 1,
+          gridTemplateColumns: 'repeat(7, 1fr)',
+        }}
+      >
+        {headers.map(header => 
+          <Card sx={{minHeight:"2em"}}>{header}</Card>
+        )}
+        {dayCards.map(dayCard => 
+          <Card sx={{minHeight:"5em", gridColumn: dayCard.dayOfWeek}} key={dayCard.key} >{dayCard.label}</Card>
+        )}
+      </Box>
+    </main>
   );
 }
 
