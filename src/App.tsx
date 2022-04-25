54ea5495abc886c5da2e6e5dd2724806d25d0d9c
Calvin <calvin@edu.ink>
2022-04-25 15:08:31 +0800

    feat: app bar and month cursor buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Box, Card } from '@mui/material';
+import { ChevronLeft, ChevronRight, Menu } from "@mui/icons-material";
+import { AppBar, Box, Button, Card, CardContent, IconButton, Toolbar, Typography } from '@mui/material';
 import moment from 'moment';
 import { useEffect, useMemo, useState } from 'react';
 import './App.css';
@@ -24,7 +25,7 @@ function App() {
 
   /** Day cards */
   const [dayCards, setDayCards] = useState<DayCard[]>([]);
-  const [activeDate, setActiveDate] = useState<Date>(new Date());
+  const [activeDate, setActiveDate] = useState<moment.Moment>(moment());
   useEffect(() => {
     const newValue: DayCard[] = [];
     const stopDate = moment(activeDate).endOf("month");
@@ -39,23 +40,69 @@ function App() {
     setDayCards(newValue)
   }, [activeDate])
 
+  const goPreviousMonth = () => {
+    const newDate = moment(activeDate).subtract(1, "month");
+    setActiveDate(newDate);
+  }
+
+  const goNextMonth = () => {
+    const newDate = moment(activeDate).add(1, "month")
+    setActiveDate(newDate);
+  }
+
+  const goToday = () => {
+    const newDate = moment();
+    setActiveDate(newDate);
+  }
+
   return (
-    <main>
-      <Box
-        sx={{
-          display: 'grid',
-          gap: 1,
-          gridTemplateColumns: 'repeat(7, 1fr)',
-        }}
-      >
-        {headers.map(header => 
-          <Card sx={{minHeight:"2em"}}>{header}</Card>
-        )}
-        {dayCards.map(dayCard => 
-          <Card sx={{minHeight:"5em", gridColumn: dayCard.dayOfWeek}} key={dayCard.key} >{dayCard.label}</Card>
-        )}
+    <Box sx={{display: "flex", flexDirection: "column"}}>
+      <AppBar position="static">
+        <Toolbar variant="regular">
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <Menu />
+          </IconButton>
+          <Typography variant="h6" color="inherit" component="div" sx={{ flexGrow: 1 }}>
+            Dollar Book
+          </Typography>
+          <IconButton color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={goPreviousMonth}>
+            <ChevronLeft />
+          </IconButton>
+          <Typography variant="h6" color="inherit" component="div" sx={{width: "10em", textAlign: "center"}}>
+            {activeDate.format("MMMM YYYY")}
+          </Typography>
+          <IconButton color="inherit" aria-label="menu" sx={{ mr: 2 }} onClick={goNextMonth}>
+            <ChevronRight />
+          </IconButton>
+          <Button color="inherit" variant="outlined" disableElevation onClick={goToday}>Today</Button>
+        </Toolbar>
+      </AppBar>
+      <Box sx={{
+        flexGrow: 1,
+        height: '100vh',
+        overflow: 'auto'
+      }}>
+        <Box
+          sx={{
+            m: 1,
+            display: 'grid',
+            gap: 1,
+            gridTemplateColumns: 'repeat(7, 1fr)'
+          }}
+        >
+          {headers.map(header => 
+            <Box sx={{textAlign: "center"}}>{header}</Box>
+          )}
+          {dayCards.map(dayCard => 
+            <Card sx={{gridColumn: dayCard.dayOfWeek}} key={dayCard.key}>
+              <CardContent>
+                {dayCard.label}
+              </CardContent>
+              </Card>
+          )}
+        </Box>
       </Box>
-    </main>
+    </Box>
   );
 }
 
